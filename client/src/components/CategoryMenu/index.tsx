0f6e6103731dce77737a9ac13c70fa7aa6ac6941
potatoes <osoriomando2345@gmail.com>
2022-07-24 16:45:52 -0700

    converted everything into typescript files, works well

---
+++ client/src/components/CategoryMenu/index.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+} from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/db/queries';
+import { idbPromise } from '../../utils/helpers';
+
+function CategoryMenu() {
+  const dispatch = useDispatch();
+  const state: any = useSelector((state) => state);
+
+  const { categories } = state;
+
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  useEffect(() => {
+    if (categoryData) {
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories,
+      });
+      categoryData.categories.forEach((category: any) => {
+        idbPromise('categories', 'put', category);
+      });
+    } else if (!loading) {
+      idbPromise('categories', 'get', {}).then((categories) => {
+        dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories,
+        });
+      });
+    }
+  }, [categoryData, loading, dispatch]);
+
+  const handleClick = (id: any) => {
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id,
+    });
+  };
+
+  return (
+    <div>
+      <h2>Choose a Category:</h2>
+      {categories.map((item: any) => (
+        <button
+          key={item._id}
+          onClick={() => {
+            handleClick(item._id);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default CategoryMenu;
